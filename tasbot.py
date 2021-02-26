ae224d8d8554afa94d098847372bb88e626cda2a
Jethro Modequillo <mjolnir311@gmail.com>
2021-02-26 20:12:00 +0800

    Added record_macro() to make new macro

--- tasbot.py
+++ tasbot.py
@@ -5,25 +5,34 @@ record_macro_key = "esc"
 
 # The button you press to play the existing macro
 play_macro_key = "ctrl+esc"
-keyboard.press_and_release('shift+s, space')
 
-keyboard.write('The quick brown fox jumps over the lazy dog.')
+def record_macro():
+    recorded=keyboard.record(until=record_macro_key)
 
 keyboard.add_hotkey('ctrl+shift+a', print, args=('triggered', 'hotkey'))
 
 # Press PAGE UP then PAGE DOWN to type "foobar".
 keyboard.add_hotkey('page up, page down', lambda: keyboard.write('foobar'))
 
-# Blocks until you press esc.
-keyboard.wait('esc')
+# keyboard.press_and_release('shift+s, space')
 
-# Record events until 'esc' is pressed.
-recorded = keyboard.record(until='esc')
-# Then replay back at three times the speed.
-keyboard.play(recorded, speed_factor=3)
+# keyboard.write('The quick brown fox jumps over the lazy dog.')
 
-# Type @@ then press space to replace with abbreviation.
-keyboard.add_abbreviation('@@', 'my.long.email@example.com')
+# keyboard.add_hotkey('ctrl+shift+a', print, args=('triggered', 'hotkey'))
 
-# Block forever, like `while True`.
-keyboard.wait()
+# # Press PAGE UP then PAGE DOWN to type "foobar".
+# keyboard.add_hotkey('page up+page down', lambda: keyboard.write('foobar'))
+
+# # Blocks until you press esc.
+# keyboard.wait('esc')
+
+# # Record events until 'esc' is pressed.
+# recorded = keyboard.record(until='esc')
+# # Then replay back at three times the speed.
+# keyboard.play(recorded, speed_factor=3)
+
+# # Type @@ then press space to replace with abbreviation.
+# keyboard.add_abbreviation('@@', 'my.long.email@example.com')
+
+# # Block forever, like `while True`.
+# keyboard.wait()
