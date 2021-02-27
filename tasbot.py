902024716bedefbfd78ba9a7db0d61b925e04327
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-27 23:18:43 +0800

    Using ONLY keyboard listener NOTE: Needs timing implementation

--- tasbot.py
+++ tasbot.py
@@ -1,10 +1,35 @@
-from pynput import mouse
+# from pynput import mouse
+# from pynput import keyboard
+
+
+# def on_click(x, y, button, pressed):
+#     did_press = "Press" if pressed else "Release"
+#     print(f"{did_press}: ({x}, {y})")
+
+
+# def on_release(key):
+#     print("{0} released".format(key))
+#     if key == keyboard.Key.esc:
+#         # Stop listener
+#         return False
+
+
+# # Collect events until released
+# with keyboard.Listener(on_release=on_release) as k_listener, mouse.Listener(
+#     on_click=on_click
+# ) as m_listener:
+#     k_listener.join()
+#     m_listener.join()
+
 from pynput import keyboard
 
 
-def on_click(x, y, button, pressed):
-    did_press = "Press" if pressed else "Release"
-    print(f"{did_press}: ({x}, {y})")
+def on_press(key):
+    try:
+        print("alphanumeric key {0} pressed".format(key.char))
+    except AttributeError:
+        print("special key {0} pressed".format(key))
+
 
 def on_release(key):
     print("{0} released".format(key))
@@ -14,7 +39,5 @@ def on_release(key):
 
 
 # Collect events until released
-with keyboard.Listener(on_release=on_release) as k_listener, \
-        mouse.Listener(on_click=on_click) as m_listener:
-    k_listener.join()
-    m_listener.join()
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
