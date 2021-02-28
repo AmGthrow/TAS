b5dc57c10f400d23b0ddac3d7921c3d02ab49650
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-28 22:21:37 +0800

    added add_event() to EventRecorder

--- EventRecorder.py
+++ EventRecorder.py
@@ -1,7 +1,17 @@
 import datetime
 import time
 
+
 class EventRecorder:
     def __init__(self):
         self.now = time.time()
         self.events = []
+
+    def add_event(self, event):
+        # record how much time has passed between the last event and the current one
+        timedelta = self.get_timedelta()
+        self.now = time.time()
+        self.events.append(timedelta, event)
+
+    def get_timedelta(self):
+        return datetime.datetime.now() - self.now
