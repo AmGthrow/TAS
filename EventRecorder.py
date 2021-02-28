3ee5de2b2a47ca7d329a11b39d7f3bc712082e86
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-28 22:27:16 +0800

    use datetime instead of time for recording self.now
    We can't perform a datetime - time operation, gotta use two datetimes
    since that's how we can get a really convenient timedelta

--- EventRecorder.py
+++ EventRecorder.py
@@ -4,14 +4,13 @@ import time
 
 class EventRecorder:
     def __init__(self):
-        self.now = time.time()
+        self.now = datetime.datetime.now()
         self.events = []
 
     def add_event(self, event):
         # record how much time has passed between the last event and the current one
         timedelta = self.get_timedelta()
-        self.now = time.time()
-        self.events.append(timedelta, event)
+        self.now = datetime.datetime.now()
 
     def get_timedelta(self):
         return datetime.datetime.now() - self.now
