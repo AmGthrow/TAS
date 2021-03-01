693522965690b0acdbb8a9a13a85ee5929ed0915
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-03-01 15:34:29 +0800

    Added logging to Events.py for both recording and executing

--- Events.py
+++ Events.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractclassmethod
 from pynput.keyboard import Controller as KeyboardController
 from pynput.mouse import Controller as MouseController
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s')
 
 keyboard = KeyboardController()
 mouse = MouseController()
@@ -10,15 +14,19 @@ class KeyboardEvent:
     class Press:
         def __init__(self, key):
             self.key = key
+            logging.info(f"Pressing {self.key}")
 
         def execute(self):
+            logging.info(f"Pressed {self.key}")
             keyboard.press(self.key)
 
     class Release:
         def __init__(self, key):
             self.key = key
+            logging.info(f"Releasing {self.key}")
 
         def execute(self):
+            logging.info(f"Released {self.key}")
             keyboard.release(self.key)
 
 
@@ -27,8 +35,10 @@ class MouseEvent:
         def __init__(self, x, y):
             self.x = x
             self.y = y
+            logging.info(f"Moving to ({self.x}, {self.y})")
 
         def execute(self):
+            logging.info(f"Moved to ({self.x}, {self.y})")
             mouse.position = (self.x, self.y)
 
     class Click:
@@ -37,6 +47,9 @@ class MouseEvent:
             self.y = y
             self.button = button
             self.pressed = pressed
+            logging.info(f"Moving to ({self.x}, {self.y})")
+            did_press = "Pressing" if self.pressed else "Releasing"
+            logging.info(f"{did_press} {self.button}")
 
         def execute(self):
             mouse.position = (self.x, self.y)
@@ -44,6 +57,9 @@ class MouseEvent:
                 mouse.press(self.button)
             else:
                 mouse.release(self.button)
+            logging.info(f"Moved to ({self.x}, {self.y})")
+            did_press = "Pressed" if self.pressed else "Released"
+            logging.info(f"{did_press} {self.button}")
 
     class Scroll:
         def __init__(self, x, y, dx, dy):
@@ -51,7 +67,31 @@ class MouseEvent:
             self.y = y
             self.dx = dx
             self.dy = dy
+            logging.info(f"Moving to ({self.x}, {self.y})")
+            direction = ""
+            if dy > 0:
+                direction += "Up"
+            elif dy < 0:
+                direction += "Down"
+
+            if dx > 0:
+                direction += "Right"
+            elif dx < 0:
+                direction += "Left"
+            logging.info(f"Scrolling {direction}: ({self.x}, {self.y})")
 
         def execute(self):
             mouse.position = (self.x, self.y)
             mouse.scroll(self.dx, self.dy)
+            logging.info(f"Moved to ({self.x}, {self.y})")
+            direction = ""
+            if dy > 0:
+                direction += "Up"
+            elif dy < 0:
+                direction += "Down"
+
+            if dx > 0:
+                direction += "Right"
+            elif dx < 0:
+                direction += "Left"
+            logging.info(f"Scrolled {direction}: ({x}, {y})")
