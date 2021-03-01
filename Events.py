424cf98f984287aec94b7fb1150f2739c5ed8fc7
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-03-01 13:10:43 +0800

    Added KeyboardEvent with Press and Release

--- Events.py
+++ Events.py
@@ -7,3 +7,21 @@ class Event(ABC):
     @abstractclassmethod
     def execute(self):
         pass
+
+
+class KeyboardEvent(Event):
+    keyboard = KeyboardController()
+
+    class Press:
+        def __init__(self, key):
+            self.key = key
+
+        def execute(self):
+            keyboard.press(key)
+
+    class Release:
+        def __init__(self, key):
+            self.key = key
+
+        def execute(self):
+            keyboard.release(key)
