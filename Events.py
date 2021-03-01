8bb29e2741aeb0f68b2dd53ccbfde9d5c7ff731e
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-03-01 14:22:02 +0800

    Use global variables for KeyboardController() and MouseController()

--- Events.py
+++ Events.py
@@ -2,29 +2,24 @@ from abc import ABC, abstractclassmethod
 from pynput.keyboard import Controller as KeyboardController
 from pynput.mouse import Controller as MouseController
 
-
-class Event(ABC):
-    @abstractclassmethod
-    def execute(self):
-        pass
+keyboard = KeyboardController()
+mouse = MouseController()
 
 
-class KeyboardEvent(Event):
-    keyboard = KeyboardController()
-
+class KeyboardEvent:
     class Press:
         def __init__(self, key):
             self.key = key
 
         def execute(self):
-            keyboard.press(key)
+            keyboard.press(self.key)
 
     class Release:
         def __init__(self, key):
             self.key = key
 
         def execute(self):
-            keyboard.release(key)
+            keyboard.release(self.key)
 
 
 class MouseEvent(Event):
