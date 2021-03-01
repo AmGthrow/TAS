36b67d30e7d09e70ea1198164c5158e3943a04b4
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-03-01 13:32:54 +0800

    put on_click, on_press, etc into TASbot class

--- tasbot.py
+++ tasbot.py
@@ -12,9 +12,9 @@ class TASbot:
         self.recording = True
 
         self.k_listener = keyboard.Listener(
-            on_release=on_release, on_press=on_press)
+            on_release=self.on_release, on_press=self.on_press)
         self.m_listener = mouse.Listener(
-            on_click=on_click, on_scroll=on_scroll)
+            on_click=self.on_click, on_scroll=self.on_scroll)
 
         self.k_listener.start()
         self.m_listener.start()
@@ -34,32 +34,28 @@ class TASbot:
         for event in self.events:
             print(event)
 
+    def on_click(self, x, y, button, pressed):
+        did_press = "Press" if pressed else "Release"
+        print(f"{did_press}: ({x}, {y})")
 
-def on_click(x, y, button, pressed):
-    did_press = "Press" if pressed else "Release"
-    print(f"{did_press}: ({x}, {y})")
+    def on_scroll(self, x, y, dx, dy):
+        direction = ""
+        if dy > 0:
+            direction += "Up"
+        elif dy < 0:
+            direction += "Down"
 
+        if dx > 0:
+            direction += "Right"
+        elif dx < 0:
+            direction += "Left"
+        print(f"{direction}: ({x}, {y})")
 
-def on_scroll(x, y, dx, dy):
-    direction = ""
-    if dy > 0:
-        direction += "Up"
-    elif dy < 0:
-        direction += "Down"
+    def on_press(self, key):
+        print(f"{key} pressed")
 
-    if dx > 0:
-        direction += "Right"
-    elif dx < 0:
-        direction += "Left"
-    print(f"{direction}: ({x}, {y})")
-
-
-def on_press(key):
-    print(f"{key} pressed")
-
-
-def on_release(key):
-    print(f"{key} released")
+    def on_release(self, key):
+        print(f"{key} released")
 
 
 recorder = TASbot()
